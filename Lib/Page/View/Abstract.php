3a8f3f3fa245b12830ff6127822b14c3f4a788cc
MikeTayC <michael.tay@blueacorn.com>
2015-07-13 17:43:52 -0400

    traded bootstrap for uikit

--- Lib/Page/View/Abstract.php
+++ Lib/Page/View/Abstract.php
@@ -27,10 +27,10 @@ abstract class Page_View_Abstract extends Core_Model_Object
     public function getAssets(){
         $assets = $this->sortAssets();
         foreach($assets as $nodeKey => $nodeValue){
-            if($nodeValue['type'] === 'css'){
-                 echo "<link href='" . '/assets/css/' . $nodeValue['file'] . "' rel='stylesheet'>";
+            if($nodeValue['type'] === 'css1'){
+                 echo "<link href='" . '/assets/css1/' . $nodeValue['file'] . "' rel='stylesheet'>";
             }
-            elseif($nodeValue['type'] === 'js'){
+            elseif($nodeValue['type'] === 'js1'){
                 echo "<script src='" . '/assets/' . $nodeValue['file'] . "'></script>";
             }
             elseif($nodeValue['type'] === 'jquery'){
--- assets/js/admin_login.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-    $('#square').click(function(){
-        $(this).hide();
-    });
-});
