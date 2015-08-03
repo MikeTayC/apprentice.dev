febbbffd005acf15d2f1b98008a4c154482cbbc4
MikeTayC <michael.tay@blueacorn.com>
2015-08-03 09:21:31 -0400

    Began refactoring lesson controller

--- App/Incubate/Controller/Lesson.php
+++ App/Incubate/Controller/Lesson.php
@@ -1,19 +1,21 @@
 <?php
 
-class Incubate_Controller_Lesson extends Incubate_Controller_Admin
+class Incubate_Controller_Lesson extends Core_Controller_Abstract
 {
 
-    public function __construct()
-    {
-        parent::__construct();
-        if(!$this->auth->isLoggedIn()) {
-            $this->redirect('Incubate','Index','indexAction');
-        }
-    }
+        /*
+         * indexAction() is responsible for loading all the created lessons
+         * Here, admins can create update delete lessons
+         * standard users can only read a list of all lessons
+         *
+         * if session variable : logged_in is false, return to login
+         * if session variable : admin_status is true, then load create and edit links
+         * if admin_status is false, then just load list of current courses
+         */
     public function indexAction()
     {
-
         $view = $this->loadLayout();
+        if(Core_Model_Session::)
         $db = Core_Model_Database::getInstance();
         $tagMap = $db->get('lesson_tag_map', array('1', '=', '1'))->results();
 
