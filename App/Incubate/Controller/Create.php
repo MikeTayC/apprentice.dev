5947cd0d7a63329ee9554a867e3d98e9cfc9c820
MikeTayC <michael.tay@blueacorn.com>
2015-08-25 10:39:05 -0400

    fixed admin view bug

--- App/Incubate/Controller/Create.php
+++ App/Incubate/Controller/Create.php
@@ -2,15 +2,17 @@
 
 class Incubate_Controller_Create extends Incubate_Controller_Abstract
 {
-
-
-    public function lessonAction()
+    public function __construct()
     {
         //check if user is logged in and admin
         $this->_checkIfUserIsLoggedIn();
         $this->_checkIfUserIsAdmin();
+    }
 
-        //load request objecgt
+    public function lessonAction()
+    {
+
+        //load request object
         $request = $this->_getRequest();
 
         /*
@@ -20,18 +22,15 @@ class Incubate_Controller_Create extends Incubate_Controller_Abstract
          */
         if ($request->isPost() && $request->hasPost('name')) {
 
-            //load  models
+            //load  model
             $lesson = Bootstrap::getModel('incubate/lesson');
 
             /*
              * set all post information,
              * if the post variable is tag, it is a comma seprated list, explode it into $tagArray
+             * and set data on the lesson model entity
              */
             $tagArray = $this->explode($request->getPost('tags'));
-
-            /*
-             * add the lesson to the database,
-             */
             foreach(array('name','description','duration') as $field) {
                 $lesson->setData($field, $request->getPost($field));
             }
@@ -41,7 +40,6 @@ class Incubate_Controller_Create extends Incubate_Controller_Abstract
 
 
             $this->_successFlash('You successfully created a lesson!');
-
             $this->_thisModuleRedirect('lesson');
         }
         //else load view
@@ -53,8 +51,6 @@ class Incubate_Controller_Create extends Incubate_Controller_Abstract
     public function tagAction()
     {
         $request = $this->_getRequest();
-        $this->_checkIfUserIsLoggedIn();
-        $this->_checkIfUserIsAdmin();
 
         /*
          * if post is set, add the created tag to the database
